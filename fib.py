1fff19a7e9d5a86b7d7f69669315702d428e7126
Srajan91 <102983643+Srajan91@users.noreply.github.com>
2022-05-01 16:20:00 +0530

    Add files via upload
    how to calculte fibonacci series
     eg ,0,1,1,2...

---
+++ fib.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun May  1 16:03:33 2022
+
+@author: Training
+"""
+
+a=0
+b=1
+n=int(input("enter  number how many times generate series:")
+print("fibonacci series")
+print(" ",a," ",b,end="");
+for i in range(n):
+    c=a+b
+    a=b
+    b=c
+    print("",c,end="");
+    
