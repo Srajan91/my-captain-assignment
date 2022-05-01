0c07fb2473a210eca35efb965edc1e8c3f4a2f63
Srajan91 <102983643+Srajan91@users.noreply.github.com>
2022-05-01 16:37:46 +0530

    Add files via upload
    to check all positive nos between range

---
+++ range.py
@@ -0,0 +1,9 @@
+start=int(input("enter the start of range :"))
+end=int(input("enter a the end of range :"))
+for num in range (start,end+1):
+    if num >0:
+        print(num,end = "  ")
+        
+              
+        
+    
